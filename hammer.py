0835f73ad5af7520b76e43fc60f48365a9c489c5
Raden Muhammad Imam <raim1104@gmail.com>
2018-06-04 11:54:21 +0700

    Update hammer.py

--- hammer.py
+++ hammer.py
@@ -1,10 +1,4 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-# python 3.3.2+ Hammer Dos Script v.1
-# by Can Yalçın
-# only for legal purpose
-
+# Python 3.3.2+ Hammer DDoS Script
 
 from queue import Queue
 from optparse import OptionParser
